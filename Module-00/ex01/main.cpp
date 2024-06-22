b04dd1a35522945c7b4d69f0f5f0e858a22dc4cc
= <=>
2024-06-22 22:26:42 +0400

    Take input from user

---
+++ Module-00/ex01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string input;
+    std::cout << "Hey! Welcome to my very awesome PhoneBook!!!" << std::endl;
+    std::cout << "Select one of the options below\n" << std::endl;
+    std::cout << "1 - ADD" << std::endl;
+    std::cout << "2 - SEARCH" << std::endl;
+    std::cout << "3 - EXIT\n" << std::endl;
+    getline(cin, input);
+    
+}

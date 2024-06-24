18412a05bfa2aa9d50e6bb89876470b58285a190
= <=>
2024-06-24 15:33:32 +0400

    Add user with validation

--- Module-00/ex01/main.cpp
+++ Module-00/ex01/main.cpp
@@ -2,40 +2,58 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-    PhoneBook phonebook(8);
-    string input;
+std::string get_input(const std::string &prompt)
+{
+    std::string input;
 
-    while (true) {
+    while (true)
+    {
+        std::cout << prompt;
+        getline(cin, input);
+
+        if (!input.empty())
+            break;
+        std::cout << "Invalid input! Please, try again\n";
+    }
+    return (input);
+}
+
+int pb(std::string &input, PhoneBook phonebook)
+{
+    while (true)
+    {
         std::cout << "Hey! Welcome to my very awesome PhoneBook!!!" << std::endl;
         std::cout << "Select one of the options below\n" << std::endl;
         std::cout << "1 - ADD" << std::endl;
         std::cout << "2 - SEARCH" << std::endl;
         std::cout << "3 - EXIT\n" << std::endl;
         getline(cin, input);
-
-        if (input == "1" || input == "ADD") {
+        if (capitalize(input) == "ADD")
+        {
             std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
 
-            std::cout << "First Name: ";
-            getline(cin, firstName);
-            std::cout << "Last Name: ";
-            getline(cin, lastName);
-            std::cout << "Nickname: ";
-            getline(cin, nickName);
-            std::cout << "Phone Number: ";
-            getline(cin, phoneNumber);
-            std::cout << "Darkest Secret: ";
-            getline(cin, darkestSecret);
+            firstName = get_input("First Name: ");
+            lastName = get_input("Last Name: ");
+            nickName = get_input("Nickname: ");
+            while (!is_number(phoneNumber))
+            {
+                phoneNumber = get_input("Phone Number: ");
+                if (!is_number(phoneNumber))
+                    std::cout << "Invalid phone number! Try again!\n";
+            }
+            darkestSecret = get_input("Darkest Secret: ");
             phonebook.add(firstName, lastName, nickName, phoneNumber, darkestSecret);
-        } else if (input == "2" || input == "SEARCH") {
-            phonebook.display();
-        } else if (input == "3" || input == "EXIT") {
-            break;
-        } else {
-            std::cout << "Invalid option. Please try again." << std::endl;
+        }
+        else if (capitalize(input) == "SEARCH")
+        {
+            // To be handled
         }
     }
+}
 
-    return 0;
+int main(int argc, char **argv) {
+    PhoneBook phonebook(8);
+    string input;
+    pb(input, phonebook);
+    return (0);
 }
--- Module-00/ex01/phonebook-app.hpp
+++ Module-00/ex01/phonebook-app.hpp
@@ -2,7 +2,11 @@
 #define PHONEBOOK_APP_HPP
 
 #include <iostream>
+#include <cctype>
 #include "contact.hpp"
 #include "phonebook.hpp"
+
+bool is_number(const std::string &str);
+std::string capitalize(std::string &input);
 
 #endif

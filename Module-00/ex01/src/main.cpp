8411065d9d66aa59a7a7de0fb5d34d8d76242501
= <=>
2024-08-20 20:54:25 +0400

    Phonebook, dream job

--- Module-00/ex01/src/main.cpp
+++ Module-00/ex01/src/main.cpp
@@ -1,7 +1,13 @@
 #include "../include/Utils.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    (void)argv;
+    if (argc != 1)
+    {
+        std::cout << "Error\nInvalid arguments!\nUsage - ./phonebook\n";
+        return (1);
+    }
     PhoneBook phoneBook;
     start(phoneBook);
     return (0);

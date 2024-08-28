31336f1134ac33378d99044f96c9a7c39ea19f1c
= <=>
2024-08-28 18:39:02 +0400

    Orthodox cannonical class

---
+++ Module-02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.hpp"
+
+const int Fixed::_num = 8;
+
+Fixed::Fixed(void) : _fixedPoint(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f) : _fixedPoint(f._fixedPoint)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed &f)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &f)
+        _fixedPoint = f.getRawBits();
+    return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_fixedPoint);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixedPoint = raw;
+}

3844d6a7b870dfaf120a42ef041a8b051745df04
= <=>
2024-09-17 20:19:09 +0400

    ex02

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -4,14 +4,14 @@ int main(void)
 {
     std::string str = "HI THIS IS BRAIN";
 
-    std::string *ptr = &str;
-    std::string &ref = str;
+    std::string *stringPtr = &str;
+    std::string &stringRef = str;
     
     std::cout << "Memory address of string - " << &str << "\n"
-              << "Memory address of pointer - "<< ptr << "\n"
-              << "Memory address of reference - " << &ref << "\n"
+              << "Memory address of pointer - "<< stringPtr << "\n"
+              << "Memory address of stringReference - " << &stringRef << "\n"
               << "Value of string - " << str << "\n"
-              << "Value of string ptr - " << *ptr << "\n"
-              << "Value of string reference - " << ref << std::endl;
+              << "Value of string stringPtr - " << *stringPtr << "\n"
+              << "Value of string stringReference - " << stringRef << std::endl;
     return (0);
 }

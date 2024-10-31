e210432fda5f68f6f348d5901736976a07905720
= <=>
2024-10-31 18:26:51 +0400

    Convertor fix

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -24,97 +24,238 @@ ScalarConverter::~ScalarConverter()
     std::cout << "ScalarConverter destroyed :((" << std::endl;
 }
 
-bool isNumber(const std::string& str) {
-    if (str.empty()) return false;
-    if (str[0] == '-') {
-        return str.length() > 1 && std::all_of(str.begin() + 1, str.end(), ::isdigit);
-    }
-    return std::all_of(str.begin(), str.end(), ::isdigit);
-}
-
-bool isFloat(const std::string& str) {
-    if (str.empty()) return false;
-
-    bool hasF = (str.back() == 'f' || str.back() == 'F');
-    std::string numericPart = hasF ? str.substr(0, str.size() - 1) : str;
-
-    char* end;
-    std::strtof(numericPart.c_str(), &end);
-    
-    return end != numericPart.c_str() && *end == '\0';
-}
-
-void toChar(const std::string& str) {
-    float f = std::stof(str);
-    int x = static_cast<int>(f);
-
-    if ((f < 0) || (x > 255))
-        std::cout << "Char: impossible" << std::endl;
-    else if (f >= 32 && f <= 126)
+std::string ft_toupper(std::string str)
+{
+    for (int i = 0; str[i]; i++)
     {
-        char r = static_cast<char>(x);
-        std::cout << "Char: " << r << std::endl;
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = (char)std::toupper(str[i]);
+    }
+    return (str);
+}
+
+bool isNum(const std::string str)
+{
+    unsigned int i = 0;
+    
+    if (str.empty())
+        return (false);
+    if (str[0] == '-') {
+        if (str.size() == 1 || !std::isdigit(str[1]))
+            return (false);
+        i = 1;
+    }
+    while (i < str.size())
+    {
+        if (!std::isdigit(str[i]))
+            return (false);
+        i++;
+    }
+    return (true);
+}
+
+bool isFloat(const std::string str)
+{
+    unsigned int i = 0;
+    bool hasDecimal = false;
+
+    if (str.empty())
+        return (false);
+    if (str[0] == '-')
+    {
+        if (str.size() == 1 || !std::isdigit(str[1]))
+            return (false);
+        i = 1;
+    }
+    while (i < str.size())
+    {
+        if (i == str.size() - 1 && str[i] == 'f')
+            return (hasDecimal);
+        if (str[i] == '.')
+        {
+            if (hasDecimal)
+                return (false);
+            hasDecimal = true;
+        }
+        else if (!std::isdigit(str[i]))
+            return (false);
+        i++;
+    }
+    return (true);
+}
+
+void processInput(std::string str)
+{
+    if (ft_toupper(str) == "-INF" || ft_toupper(str) == "-INFF")
+    {
+        std::cout << "char: imposibble\nint: imposibble\nfloat: -inff\ndouble: -inf" << std::endl;
+        exit(1);
+    }
+    else if (ft_toupper(str) == "+INF" || ft_toupper(str) == "+INFF")
+    {
+        std::cout << "char: imposibble\nint: imposibble\nfloat: +inff\ndouble: +inf" << std::endl;
+        exit(1);
+    }
+    else if (ft_toupper(str) == "NAN" || ft_toupper(str) == "NANF")
+    {
+        std::cout << "char: imposibble\nint: imposibble\nfloat: nanf\ndouble: nan" << std::endl;
+        exit(1);
+    }
+    else if (!isNum(str) && !isFloat(str) && str.size() > 1)
+    {
+        std::cout << "char: imposibble\nint: imposibble\nfloat: imposibble\ndouble: imposibble" << std::endl;
+        exit(1);
+    }
+}
+
+void toChar(std::string str)
+{
+    int x;
+    float f;
+
+    if (isNum(str))
+    {
+        x = std::atoi(str.c_str());
+        char c = static_cast<char>(x);
+        if (x >= 32 && x <= 126)
+            std::cout << "char: '" << c << "'" << std::endl;
+        else if (x < 0 || x > 255)
+            std::cout << "char: imposibble" << std::endl;
+        else
+            std::cout << "char: non displayable" << std::endl;
+    }
+    else if (isFloat(str))
+    {
+        f = std::stof(str);
+        x = static_cast<int>(f);
+        char c = static_cast<char>(x);
+        if (f < 0 || x > 255)
+            std::cout << "char: imposibble" << std::endl;
+        else if (x >= 32 && x <= 126)
+            std::cout << "char: '" << c << "'" << std::endl;
+        else
+            std::cout << "char: non displayable" << std::endl;
     }
     else
-        std::cout << "Char: Non displayable" << std::endl;
-    
-    std::cout << "----------------------------" << std::endl;
+    {
+        if (str.size() == 1)
+            std::cout << "char: '" << str[0] << "' " << std::endl;
+        else
+            std::cout << "char: impossible" << std::endl;
+    }
 }
 
-void toInt(const std::string& str) {
-    if (isFloat(str))
+void toInt(std::string str)
+{
+    int x;
+    long long temp;
+    if (!isNum(str) && !isFloat(str))
     {
-        float f = std::stof(str);
-        if (f < std::numeric_limits<int>::min() || f > std::numeric_limits<int>::max()) {
-            std::cout << "Int: impossible" << std::endl;
+        if (str.size() == 1)
+        {
+            x = static_cast<int>(str[0]);
+            std::cout << "int: " << x << std::endl;
+            return;
         }
         else
         {
-            int x = static_cast<int>(f);
-            std::cout << "Int: " << x << std::endl;
+            std::cout << "int: invalid input" << std::endl;
+            return;
         }
     }
-    else if (isNumber(str))
+    try
     {
-        int x = std::atoi(str.c_str());
-        std::cout << "Int: " << x << std::endl;
+        if (isNum(str))
+            temp = std::stoll(str);
+        if (isFloat(str))
+            temp = std::stof(str);
+        if (temp > std::numeric_limits<int>::max() || temp < std::numeric_limits<int>::min())
+            std::cout << "int: impossible" << std::endl;
+        else
+        {
+            x = static_cast<int>(temp);
+            std::cout << "int: " << x << std::endl;
+        }
     }
-    else
-        std::cout << "Int: impossible" << std::endl;
-
-    std::cout << "----------------------------" << std::endl;
+    catch (const std::out_of_range&)
+    {
+        std::cout << "int: impossible" << std::endl;
+    }
 }
 
-void toFloat(const std::string& str) {
-    if (!isFloat(str))
+void toFloat(std::string str)
+{
+    float x;
+    long long temp;
+    if (!isNum(str) && !isFloat(str))
     {
-        std::cout << "Float: impossible" << std::endl;
-        std::cout << "----------------------------" << std::endl;
-        return;
+        if (str.size() == 1)
+        {
+            x = static_cast<float>(str[0]);
+            std::cout << std::fixed << std::setprecision(1) << "float: " << x << "f" << std::endl;
+            return;
+        }
+        else
+        {
+            std::cout << "float: invalid input" << std::endl;
+            return;
+        }
     }
-
-    float f = std::stof(str);
-    std::cout << std::fixed << std::setprecision(1) << "Float: " << f << "f" << std::endl;
-
-    std::cout << "----------------------------" << std::endl;
+    try
+    {
+        temp = std::stof(str);
+        if (temp > std::numeric_limits<int>::max() || temp < std::numeric_limits<int>::min())
+            std::cout << "float: impossible" << std::endl;
+        else
+        {
+            x = static_cast<float>(temp);
+            std::cout << std::fixed << std::setprecision(1) << "float: " << x << "f" << std::endl;
+        }
+    }
+    catch (const std::out_of_range&)
+    {
+        std::cout << "float: impossible" << std::endl;
+    }
 }
 
-void toDouble(const std::string& str) {
-    if (str == "nan" || str == "NAN")
-        std::cout << "Double: nan" << std::endl;
-    else if (!isFloat(str))
-        std::cout << "Double: impossible" << std::endl;
-    else
+void toDouble(std::string str)
+{
+    double x;
+    long long temp;
+    if (!isNum(str) && !isFloat(str))
     {
-        double d = std::stod(str); // Convert string to double
-        std::cout << std::fixed << std::setprecision(1) << "Double: " << d << std::endl;
+        if (str.size() == 1)
+        {
+            x = static_cast<float>(str[0]);
+            std::cout << "float: " << x << std::endl;
+            return;
+        }
+        else
+        {
+            std::cout << "float: invalid input" << std::endl;
+            return;
+        }
     }
-
-    std::cout << "----------------------------" << std::endl;
+    try
+    {
+        temp = std::stof(str);
+        if (temp > std::numeric_limits<int>::max() || temp < std::numeric_limits<int>::min())
+            std::cout << "float: impossible" << std::endl;
+        else
+        {
+            x = static_cast<float>(temp);
+            std::cout << "float: " << x << std::endl;
+        }
+    }
+    catch (const std::out_of_range&)
+    {
+        std::cout << "float: impossible" << std::endl;
+    }
 }
 
 void ScalarConverter::convert(std::string str)
 {
+    processInput(str);
     toChar(str);
     toInt(str);
     toFloat(str);
